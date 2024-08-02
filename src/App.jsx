9a53ce09bdf6ea9afe039eabf97f44b5abfbbb31
Maria Maksymenko <136785612+Maksikd62@users.noreply.github.com>
2024-08-02 14:15:46 +0300

    dark_light Theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import React, { useEffect } from "react";
 import Navbar from "./components/navbar/navbar"
 
+import { ThemeProvider } from "@mui/material/styles";
+import lightTheme from "./theming/lightTheme";
+import darkTheme from "./theming/darkTheme";
+import { useSelector } from "react-redux";
+
 import "./App.css";
 
 function App() {
+  const { theme } = useSelector((state) => state.themingReducer);
+  const currentTheme = theme === "dark" ? darkTheme : lightTheme;
  
   return (
-    <>
-    <Navbar/>
-    </>
+    <ThemeProvider theme={currentTheme}>
+      <Navbar/>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
